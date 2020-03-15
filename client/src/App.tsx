a86c2fb8eb819ed325cf336112af790bf2336d4f
snipspin <photonenimpuls@gmail.com>
2020-03-15 15:37:38 -0700

    added router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import logo from './logo.svg'
+import {BrowserRouter} from 'react-router-dom'
 
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 
-import './App.css';
+import './App.css'
 import './css/style.css'
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
