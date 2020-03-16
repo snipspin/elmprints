8b89a6ef281afa8642e7b5c560d653a1eea4d4f0
chris-jburke <cjburke04@gmail.com>
2020-03-16 14:53:05 -0700

    fixed a styling issue on profile address form and change the form to a column layout

--- client/src/ProfileAddressForm.tsx
+++ client/src/ProfileAddressForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box, Input, InputLabel, FormControl, Button} from '@material-ui/core'
+import {Grid, Box, Input, InputLabel, FormControl, Button} from '@material-ui/core'
 export interface AddressProps {
 	display: boolean
 	onSubmit(): any
@@ -10,37 +10,66 @@ const ProfileAddressForm: React.FC<AddressProps> = (props) => {
 		props.onSubmit()
 	}
 	return (
-		<div>
-		<Box className="address-form">
-			<FormControl>
-				<InputLabel htmlFor="addressFullName">Full Name:</InputLabel>
-				<Input id="fullName" name="addressFullName" aria-describedby="full name form" />
-			</FormControl>
-			<FormControl>
-				<InputLabel htmlFor="addrLineOne">Address Line 1:</InputLabel>
-				<Input name="addrLineOne" aria-describedby="address line one form" />
-			</FormControl>
-			<FormControl>
-				<InputLabel htmlFor="addrLineTwo">Address Line 2:</InputLabel>
-				<Input name="addrLineTwo" aria-describedby="address line two form" />
-			</FormControl>
-			<FormControl>
-				<InputLabel htmlFor="addrCity">City:</InputLabel>
-				<Input name="addrCity" aria-describedby="address city form" />
-			</FormControl>
-			<FormControl>
-				<InputLabel htmlFor="addrState">State:</InputLabel>
-				<Input name="addrState" aria-describedby="address state form" />
-			</FormControl>
-			<FormControl>
-				<InputLabel htmlFor="addrZip">Zip Code:</InputLabel>
-				<Input name="addrZip" aria-describedby="address zip code form" />
-			</FormControl>
 
-			
-		</Box>
-		<Button variant="contained" color="primary" onClick={(e) => handleSubmit(e)}>Submit</Button>
-		</div>
+		<Grid
+            container
+            spacing={2}
+            direction="column"
+            justify="space-evenly"
+            alignItems="center"
+        >
+        	<Grid item xs={12}>
+        		<Box>
+					<FormControl>
+						<InputLabel htmlFor="addressFullName">Full Name:</InputLabel>
+						<Input id="fullName" name="addressFullName" aria-describedby="full name form" />
+					</FormControl>
+				</Box>
+			</Grid>
+			<Grid item xs={12}>
+				<Box>
+					<FormControl>
+						<InputLabel htmlFor="addrLineOne">Address Line 1:</InputLabel>
+						<Input name="addrLineOne" aria-describedby="address line one form" />
+					</FormControl>
+				</Box>
+			</Grid>
+			<Grid item xs={12}>
+				<Box>
+					<FormControl>
+						<InputLabel htmlFor="addrLineTwo">Address Line 2:</InputLabel>
+						<Input name="addrLineTwo" aria-describedby="address line two form" />
+					</FormControl>
+				</Box>
+			</Grid>
+			<Grid item xs={12}>
+				<Box>
+					<FormControl>
+						<InputLabel htmlFor="addrCity">City:</InputLabel>
+						<Input name="addrCity" aria-describedby="address city form" />
+					</FormControl>
+				</Box>
+			</Grid>
+			<Grid item xs={12}>
+				<Box>
+					<FormControl>
+						<InputLabel htmlFor="addrState">State:</InputLabel>
+						<Input name="addrState" aria-describedby="address state form" />
+					</FormControl>
+				</Box>
+			</Grid>
+			<Grid item xs={12}>
+				<Box>
+					<FormControl>
+						<InputLabel htmlFor="addrZip">Zip Code:</InputLabel>
+						<Input name="addrZip" aria-describedby="address zip code form" />
+					</FormControl>
+				</Box>
+			</Grid>
+			<Grid item xs={12}>
+				<Button variant="contained" color="primary" onClick={(e) => handleSubmit(e)}>Submit</Button>
+			</Grid>
+		</Grid>
 	)
 }
 export default ProfileAddressForm
