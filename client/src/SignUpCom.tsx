34ee5e21d78c2f57babfbb51056bf59ef2d97c79
chris-jburke <cjburke04@gmail.com>
2020-03-17 11:23:44 -0700

    added states to sign up page and fetch

--- client/src/SignUpCom.tsx
+++ client/src/SignUpCom.tsx
@@ -1,31 +1,83 @@
-import React from 'react'
+import React, {useEffect, useState, ChangeEvent} from 'react'
 import {Box, FormControl, Input, InputLabel, Button} from '@material-ui/core'
-    const SignUpCom: React.FC<any> = (props) => {
-        return(
-            <Box className="sign-up-box">
-                <Box className="sign-in-from-sign-up">
-            	    Already have an account? Login here:
-            	    <Button variant="contained" color="primary" className="sign-up-to-sign-in">Sign In</Button>
-                </Box>
-            	<FormControl>
-            		<InputLabel htmlFor="first-name">First Name:</InputLabel>
-            		<Input id="first-name" name="first-name" aria-describedby="first-name-form" />
-            	</FormControl>
-                <FormControl>
-                    <InputLabel htmlFor="last-name">Last Name:</InputLabel>
-                    <Input id="last-name" name="last-name" aria-describedby="last-name-form" />
-                </FormControl>                
-                <FormControl>
-                    <InputLabel htmlFor="email">Email:</InputLabel>
-                    <Input id="email" name="email" aria-describedby="email-form" />
-                </FormControl>
-                <FormControl>
-                    <InputLabel htmlFor="password">Password:</InputLabel>
-                    <Input type="password" name="password" aria-describedby="password-form" />
-                </FormControl>
-                <Button variant="contained" color="primary" className="submit-button">Sign Up</Button>
-            </Box>
+import {Decoded} from './App'
+import {Redirect} from 'react-router-dom'
+export interface SignupProps {
+    user: Decoded | null,
+    updateUser: (newToken: string | null) => void
+}
 
-        )
+const SignUpCom: React.FC<SignupProps> = (props) => {
+    let [email, setEmail] = useState('')
+    let [firstname, setFirstname] = useState('')
+    let [lastname, setLastname] = useState('')
+    let [message, setMessage] = useState('')
+    let [password, setPassword] = useState('')
+
+    useEffect(() => {
+        setMessage('')
+    }, [firstname, lastname, email, password])
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        console.log('in handle submit function')
+        let data: object = {
+            email,
+            firstname,
+            lastname,
+            password
+        }
+        fetch(`${process.env.REACT_APP_SERVER_URL}/auth/`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then ((response: Response) => {
+            response.json().then(result => {
+                if(response.ok) {
+                    props.updateUser(result.token)
+                } else {
+                    setMessage(`${response.status} ${response.statusText}: ${result.message}`)
+                }
+            }).catch( (err: Error) => console.log(err))
+        }).catch( (err: Error) => {
+            console.log('Error', err)
+            setMessage(`Error: ${err.toString()}`)
+        })
     }
+    if(props.user) {
+        return <Redirect to="/profile" />
+    }
+
+    return(
+        <Box className="sign-up-box">
+            <Box className="sign-in-from-sign-up">
+                Already have an account? Login here:
+                <Button variant="contained" color="primary" className="sign-up-to-sign-in">Sign In</Button>
+            </Box>
+            <FormControl>
+                <InputLabel htmlFor="firstname">First Name:</InputLabel>
+                <Input id="first-name" name="firstname" aria-describedby="first-name-form" 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstname(e.currentTarget.value)} required />
+            </FormControl>
+            <FormControl>
+                <InputLabel htmlFor="last-name">Last Name:</InputLabel>
+                <Input id="last-name" name="lastname" aria-describedby="last-name-form" 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setLastname(e.currentTarget.value)} required />
+            </FormControl>                
+            <FormControl>
+                <InputLabel htmlFor="email">Email:</InputLabel>
+                <Input id="email" name="email" aria-describedby="email-form" 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)} required />
+            </FormControl>
+            <FormControl>
+                <InputLabel htmlFor="password">Password:</InputLabel>
+                <Input type="password" name="password" aria-describedby="password-form" 
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)} required/>
+            </FormControl>
+            <Button onClick={e => handleSubmit(e)} variant="contained" color="primary" className="submit-button">Sign Up</Button>
+        </Box>
+    )
+}
 export default SignUpCom
