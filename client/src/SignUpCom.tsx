9cd689297d3a7a272e078345fa68cd37ced38df1
Pete Fitton <petefitton@Pete-Fittons-MacBook.local>
2020-03-18 15:58:38 -0700

    Deleted unnecessary changes

--- client/src/SignUpCom.tsx
+++ client/src/SignUpCom.tsx
@@ -27,8 +27,7 @@ const SignUpCom: React.FC<SignupProps> = (props) => {
             lastname,
             password
         }
-        let url = `${process.env.REACT_APP_SERVER_URL}/auth/signup`
-        fetch(url, {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/auth/signup`, {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
