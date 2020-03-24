9f6404e20912e52e1eba259bed0d3666d11f6aa6
snipspin <photonenimpuls@gmail.com>
2020-03-24 10:05:06 -0700

    fixed indentation, added type to FAQPageCom

--- client/src/Content.tsx
+++ client/src/Content.tsx
@@ -41,29 +41,30 @@ const Content: React.FC<ContentProps> = (props) => {
 
 	let [currentProduct, setCurrentProduct] = useState<ProductInformation>({title: '', sourceID:'', imageID:'', imagePath:'', price: ''})
 	const classes = useStyles();
-		return(
-				<div className={classes.root + " main"}>
-						<MuiThemeProvider theme={contentTheme}>
-								<Switch>
-									<Route exact path="/" render={() => <PosterGalleryCom currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} />} />
-									<Route path="/signup" render={() => <SignUpCom user={props.user} updateUser={props.updateUser} />} />
-									<Route path="/login" render={() => <SignInWindowCom user={props.user} updateUser={props.updateUser} />} />
-									<Route path="/posters/cart/payment" render={() => <PaymentPageCom updateUser={props.updateUser} currentProduct={currentProduct} user={props.user} />} />
-									<Route path="/posters/:id" render={() => <ProductDetail user={props.user} updateUser={props.updateUser} currentProduct={currentProduct} />} />
-									<Route path="/posters" render={() => <PosterGalleryCom currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} />} />
-									<Route path="/art/:id" component={ProductDetail} />
-									<Route path="/art" render={() => <ArtGalleryCom currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} />} />
-									<Route path="/cart/payment" render={() => <PaymentPageCom updateUser={props.updateUser} currentProduct={currentProduct} user={props.user} />} />
-									<Route path="/cart/receipt" render={() => <ReceiptPageCom currentProduct={currentProduct} user={props.user} updateUser={props.updateUser} />} />
-									<Route path="/cart" render={() => <ShowCartPageCom updateUser={props.updateUser} currentProduct={currentProduct} user={props.user} />} />
-									<Route path="/profile" render={() => <ProfilePageCom user={props.user} updateUser={props.updateUser} />} />
-									<Route path="/faq" component={FAQPageCom} />
-									<Route path="/search" render={() => <SearchResultsCom currentProduct={currentProduct} searchTerm={props.searchTerm} setCurrentProduct={setCurrentProduct} />} />
-									<Route path="/purchases" render={() => <PurchaseHistory user={props.user} updateUser={props.updateUser} />} />
-								</Switch>
-						</MuiThemeProvider>
-				</div>
-		)
+	
+	return(
+		<div className={classes.root + " main"}>
+			<MuiThemeProvider theme={contentTheme}>
+					<Switch>
+						<Route exact path="/" render={() => <PosterGalleryCom currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} />} />
+						<Route path="/signup" render={() => <SignUpCom user={props.user} updateUser={props.updateUser} />} />
+						<Route path="/login" render={() => <SignInWindowCom user={props.user} updateUser={props.updateUser} />} />
+						<Route path="/posters/cart/payment" render={() => <PaymentPageCom updateUser={props.updateUser} currentProduct={currentProduct} user={props.user} />} />
+						<Route path="/posters/:id" render={() => <ProductDetail user={props.user} updateUser={props.updateUser} currentProduct={currentProduct} />} />
+						<Route path="/posters" render={() => <PosterGalleryCom currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} />} />
+						<Route path="/art/:id" component={ProductDetail} />
+						<Route path="/art" render={() => <ArtGalleryCom currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} />} />
+						<Route path="/cart/payment" render={() => <PaymentPageCom updateUser={props.updateUser} currentProduct={currentProduct} user={props.user} />} />
+						<Route path="/cart/receipt" render={() => <ReceiptPageCom currentProduct={currentProduct} user={props.user} updateUser={props.updateUser} />} />
+						<Route path="/cart" render={() => <ShowCartPageCom updateUser={props.updateUser} currentProduct={currentProduct} user={props.user} />} />
+						<Route path="/profile" render={() => <ProfilePageCom user={props.user} updateUser={props.updateUser} />} />
+						<Route path="/faq" component={FAQPageCom} />
+						<Route path="/search" render={() => <SearchResultsCom currentProduct={currentProduct} searchTerm={props.searchTerm} setCurrentProduct={setCurrentProduct} />} />
+						<Route path="/purchases" render={() => <PurchaseHistory user={props.user} updateUser={props.updateUser} />} />
+					</Switch>
+			</MuiThemeProvider>
+		</div>
+	)
 }
 
 export default Content
